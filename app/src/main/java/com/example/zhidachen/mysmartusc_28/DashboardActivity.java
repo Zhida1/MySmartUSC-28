78e157d803c12deec39c128763654eb3824899a9
zhidachen <zhidache@usc.edu>
2018-10-22 09:23:08 -0700

    1.Connect MainActivity to LoginActivity 2.Add credentials file for Gmail Api

--- app/src/main/java/com/example/zhidachen/mysmartusc_28/DashboardActivity.java
+++ app/src/main/java/com/example/zhidachen/mysmartusc_28/DashboardActivity.java
@@ -5,7 +5,6 @@ import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
-import android.view.View.OnClickListener;
 import android.view.ViewGroup;
 import android.widget.Button;
 
@@ -36,6 +35,7 @@ public class DashboardActivity extends Fragment implements View.OnClickListener 
             case R.id.to_add_keyword:
                 MainActivity.fragmentManager.beginTransaction().replace(R.id.fragment_container,new PreferenceActivity()).addToBackStack(null).commit();
                 break;
+
         }
     }
 }
--- app/src/main/java/com/example/zhidachen/mysmartusc_28/MainActivity.java
+++ app/src/main/java/com/example/zhidachen/mysmartusc_28/MainActivity.java
@@ -1,17 +1,12 @@
 package com.example.zhidachen.mysmartusc_28;
 
 import android.arch.persistence.room.Room;
+import android.content.Intent;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
-import android.support.design.widget.BottomNavigationView;
 import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
-import android.view.MenuItem;
-import android.widget.Adapter;
-import android.widget.ArrayAdapter;
-import android.widget.Spinner;
-import android.widget.TextView;
-import android.widget.Toast;
+import android.view.View;
+import android.widget.Button;
 
 import java.util.List;
 
@@ -19,6 +14,7 @@ public class MainActivity extends AppCompatActivity {
     public static AppDatabase appDatabase;
     public static FragmentManager fragmentManager;
     public static User usr;
+    public Button redirect_loginBn;
 
 
 
@@ -38,6 +34,16 @@ public class MainActivity extends AppCompatActivity {
             }
         }
         fragmentManager.beginTransaction().add(R.id.fragment_container, new DashboardActivity()).commit();
+
+
+        redirect_loginBn = (Button) findViewById(R.id.Login_Button);
+        redirect_loginBn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent toLoginActivityIntent = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(toLoginActivityIntent);
+            }
+        });
 
     }
 
