77111a5d0a95476bb0964fa8711e3b647cf58078
zhidachen <zhidache@usc.edu>
2018-10-30 13:04:10 -0700

    Added greeting message with user first name (work only for first time login)

--- app/src/main/java/com/example/zhidachen/mysmartusc_28/DashboardActivity.java
+++ app/src/main/java/com/example/zhidachen/mysmartusc_28/DashboardActivity.java
@@ -2,7 +2,7 @@ package com.example.zhidachen.mysmartusc_28;
 
 
 import android.app.Activity;
-import android.content.Context;
+import android.arch.persistence.room.Room;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
@@ -13,6 +13,7 @@ import android.widget.Button;
 import android.widget.TextView;
 
 import java.util.ArrayList;
+import java.util.List;
 
 
 /**
@@ -22,6 +23,7 @@ public class DashboardActivity extends Fragment implements View.OnClickListener 
     public Button addKeywordBn;
     public Button viewKeywordsBn;
     public TextView displayNotification;
+    public static AppDatabase appDatabase;
 
     public DashboardActivity() {
         // Required empty public constructor
@@ -32,14 +34,24 @@ public class DashboardActivity extends Fragment implements View.OnClickListener 
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-
         View view = inflater.inflate(R.layout.dashboard_activity, container, false);
+
+        // settings for greeting message
+        AppDatabase appDatabase = Room.databaseBuilder(getContext(), AppDatabase.class, "userDatabase").allowMainThreadQueries().build();
+        List<User> users = appDatabase.appDao().getUsers();
+        TextView helloUser = (TextView) view.findViewById(R.id.hello_user);
+        String greeting = "Hello " + users.get(0).getUsername();
+        helloUser.setText(greeting);
+
+        // settings for add keyword button
         addKeywordBn = view.findViewById(R.id.to_add_keyword);
         addKeywordBn.setOnClickListener(this);
 
+        // settings for view keyword button
         viewKeywordsBn = view.findViewById(R.id.to_view_keywords);
         viewKeywordsBn.setOnClickListener(this);
 
+        // settings for display notification
         displayNotification = view.findViewById(R.id.display_notification);
         ArrayList<Notification> keywords = MainActivity.usr.getNotifications();
         String info = "";
